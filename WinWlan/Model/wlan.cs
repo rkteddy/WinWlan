a61e6978a5836ed4ecc4ca14f9255fc6aa194240
RkTeddy <838474413@qq.com>
2015-08-15 16:41:01 +0800

    Wlan list model

---
+++ WinWlan/Model/wlan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinWlan.Model
+{
+    class Wlan
+    {
+        public string name { get; set; }
+        public string ssid { get; set; }
+        public string rssi { get; set; }
+    }
+}
