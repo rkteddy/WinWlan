38577362b6e81efc8af620e6aff25532c9326cca
RkTeddy <838474413@qq.com>
2015-08-15 16:46:49 +0800

    Wlan list view-model

---
+++ WinWlan/ViewModel/WlanListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using WinWlan.Model;
+
+namespace WinWlan.ViewModel
+{
+    class WlanListViewModel
+    {
+        public ObservableCollection<Wlan> List { get; private set; }
+
+        public WlanListViewModel()
+        {
+            
+        }
+    }
+}
